ad1b3383fd9e3659b234a01fa176b15dcd21efd9
GabiCodings <shfasj@gmail>
2025-05-21 21:12:44 -0300

    nao estou mais desenhando as imagens, adicionei uma imagem feita no canva para o fundo

--- FocusFluir.py
+++ FocusFluir.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+from PIL import Image, ImageTk
 
 def desenhar_nuvem(x, y):
     cor = 'white'
@@ -17,10 +18,11 @@ def redesenhar_nuvens(e):
     largura_tela =  e.width
     altura_tela = e.height
     canvas.create_rectangle(0,0,largura_tela,altura_tela,fill='light blue')
-    for n in range(10):
+    for n in range(12):
         nx = random.randrange(0,largura_tela)
         ny = random.randrange(0,altura_tela)
         desenhar_nuvem(nx, ny)
+
 
 
 janela = tk.Tk()
@@ -34,9 +36,22 @@ altura_tela = janela.winfo_screenheight()
 
 print(largura_tela, altura_tela)
 
-canvas = tk.Canvas(janela, width=largura_tela, height=altura_tela, bg='light blue', highlightthickness=0)
-canvas.pack()
-janela.bind("<Configure>", redesenhar_nuvens)
 
+image = Image.open("sky.jpg.png")
+image = image.resize((largura_tela, altura_tela), Image.LANCZOS)
+background_image = ImageTk.PhotoImage(image)
+
+background_label = tk.Label(janela, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# Keep a reference to the image to prevent garbage collection
+background_label.image = background_image
+
+#canvas = tk.Canvas(janela, width=largura_tela, height=altura_tela, bg='light blue', highlightthickness=0)
+#canvas.grid(0,0)
+#janela.bind("<Configure>", redesenhar_nuvens)
+
+label_email = tk.Label(janela, text="E-mail:", font=("Arial", 12))
+label_email.grid(row=1, column=1, sticky="e", padx=10, pady=10)
 
 janela.mainloop()
