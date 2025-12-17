8e00df48eba1e211b8e2e2e976d6792ad274325d
GabiCodings <Gabi.email>
2025-12-17 06:59:08 -0300

    eu declaro que está quase pronto

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=c:\Users\GABRIELASCHUMACHERNE\Documents\FocusFluir\.venv"
+set "VIRTUAL_ENV=D:\Gabriela\VsCode\FocusFluir\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
